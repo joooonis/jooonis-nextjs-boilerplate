87437f3831e1df330fc9257191ff21df7caeb234
jooooonis <xowns3213@naver.com>
2022-11-06 18:15:35 +0900

    fix : verson downgrade

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>Home</div>;
+  return <div className="bg-orange-500">Home</div>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,jsx,ts,tsx}',
-    './components/**/*.{js,jsx,ts,tsx}',
+    './src/pages/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
